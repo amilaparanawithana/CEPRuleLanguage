4ece5cb05eb70943a591e9d4054ab2c038df6d1e
Amila Paranawithana <amila1204@gmail.com>
2016-11-11 12:36:21 +0530

    Adding package-info

---
+++ Rule-Parser/src/main/java/uom/msc/cse/api/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package includes user exposed APIs which exposes language passing methods
+ */
+package uom.msc.cse.api;
+
---
+++ Rule-Parser/src/main/java/uom/msc/cse/impl/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package includes Language parsing algorithem implementation
+ */
+package uom.msc.cse.impl;
+
